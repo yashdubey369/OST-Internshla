216d58aea4889192a9f7f1c55e6681ef86938475
YASH DUBEY <100083269+yashdubey369@users.noreply.github.com>
2024-02-27 00:23:09 +0530

    Add files via upload

---
+++ precache-manifest.628e6de8f77c118992fedd905b9f8f86.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e84ce8da5909835ad55ffbe2e88f129",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d9b317d1f2e3f7f3a02",
+    "url": "/static/css/main.4c1862c1.chunk.css"
+  },
+  {
+    "revision": "e57aa03332bd53698226",
+    "url": "/static/js/2.15a7097f.chunk.js"
+  },
+  {
+    "revision": "6a3fd5f48a97c7577e9c3ede94b4a5b5",
+    "url": "/static/js/2.15a7097f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d9b317d1f2e3f7f3a02",
+    "url": "/static/js/main.5491db42.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.5491db42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f620b634b443fc7d2dfb",
+    "url": "/static/js/runtime-main.7e6e88a6.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/static/media/memories.5c0c63fb.png"
+  }
+]);
